a65f8875ebcd3b87c40d7d34a452b55db78b6286
PurpleGrape5014 <39256185+PurpleGrape5014@users.noreply.github.com>
2020-09-12 17:54:02 -0700

    update feb bid

--- OCBUS-Original.sql
+++ OCBUS-Original.sql
@@ -1968,5 +1968,5 @@ INSERT INTO trips(route_id, agency_id, service_days, trip_id, times, trip_headsi
 --Select * FROM routes;
 --Select * FROM trips;
 --Select route_id, times, trip_headsign, block_id FROM trips WHERE trip_headsign = 'LONG BEACH - CAL STATE LONG BEACH' ORDER BY times;--
-Select times, route_id, trip_id, trip_headsign, block_id FROM trips WHERE block_id = '5016' ORDER BY times;
+Select times, route_id, trip_id, trip_headsign, block_id FROM trips WHERE block_id = '5044' ORDER BY times;
 --Select route_id, times, trip_headsign, block_id FROM trips WHERE route_id = '50' ORDER BY times;
